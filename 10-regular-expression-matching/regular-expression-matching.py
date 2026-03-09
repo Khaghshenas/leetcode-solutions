1db0ea76e5faf1aaa2f7662b824ac574d856b673
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-09 12:04:21 +0100

    Time: 7 ms (61.37%) | Memory: 19.2 MB (96.62%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,13 +1,15 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if not p:
-            return not s
+        m, n = len(s), len(p)
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        dp[m][n] = True
 
-        first_c_match = bool(s) and (s[0]==p[0] or p[0]==".")
+        for i in range(m, -1, -1):
+            for j in range(n-1, -1, -1):
+                first_match = i < m and (s[i] == p[j] or p[j] == '.')
+                if j + 1 < n and p[j+1] == '*':
+                    dp[i][j] = dp[i][j+2] or (first_match and dp[i+1][j])
+                else:
+                    dp[i][j] = first_match and dp[i+1][j+1]
 
-        if len(p)>=2 and p[1]=="*":
-            return (self.isMatch(s, p[2:])) or (first_c_match and self.isMatch(s[1:], p))
-        else:
-            return first_c_match and self.isMatch(s[1:], p[1:])
-
-        
+        return dp[0][0]
