6f1e414e8b2fc87cd2e43c5f366c568c051aa7df
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-11 11:06:04 +0100

    Time: 31 ms (16.85%) | Memory: 23.8 MB (6.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,26 +5,48 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        k = len(lists)
+        if k==0:
+            return None
+        if k==1:
+            return lists[0]
+    
+        mid = k//2
+        left_merged = self.mergeKLists(lists[:mid])
+        right_merged = self.mergeKLists(lists[mid:])
+
+        return self.merge_two_lists(left_merged, right_merged)
         
-        pointers = [lists[i] for i in range(len(lists))]
+    def merge_two_lists(self, left: ListNode, right: ListNode) -> ListNode:
+
         dummy = ListNode(0)
         tail = dummy
 
-        while any(pointers):
+        while left or right:
 
-            min_val = float('inf')
-            min_i = -1
+            if left and right:
+                if left.val <= right.val:
+                    node = ListNode(left.val, None)
+                    tail.next = node
+                    tail = tail.next
+                    left = left.next
+                else:
+                    node = ListNode(right.val, None)
+                    tail.next = node
+                    tail = tail.next
+                    right = right.next
+            elif left:
+                while left:
+                    tail.next = left
+                    tail = tail.next
+                    left = left.next
+            else:
+                while right:
+                    tail.next = right
+                    tail = tail.next
+                    right = right.next
+        
+        return dummy.next
 
-            for i in range(len(pointers)):
-                if pointers[i] and pointers[i].val < min_val:
-                    min_val = pointers[i].val
-                    min_i = i
-                
-            pointers[min_i] = pointers[min_i].next
-            
-            node = ListNode(min_val, None)
-            tail.next = node
-            tail = tail.next
 
-        return dummy.next   
         
