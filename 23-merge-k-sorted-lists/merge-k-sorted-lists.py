e27366f59824d5e4dc072c9909791fed60a7ae0a
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-11 20:25:30 +0100

    Time: 35 ms (15.94%) | Memory: 24 MB (5.25%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -10,43 +10,51 @@ class Solution:
             return None
         if k==1:
             return lists[0]
-    
-        mid = k//2
-        left_merged = self.mergeKLists(lists[:mid])
-        right_merged = self.mergeKLists(lists[mid:])
-
-        return self.merge_two_lists(left_merged, right_merged)
         
-    def merge_two_lists(self, left: ListNode, right: ListNode) -> ListNode:
+        mid = k//2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
 
+        return self.two_merge(left, right) 
+    
+    def two_merge(self, left: ListNode, right: ListNode) -> ListNode:
+        if not left:
+            return right
+        if not right:
+            return left
+        
         dummy = ListNode(0)
         tail = dummy
 
         while left or right:
-
-            if left and right:
-                if left.val <= right.val:
-                    node = ListNode(left.val, None)
-                    tail.next = node
-                    tail = tail.next
-                    left = left.next
-                else:
-                    node = ListNode(right.val, None)
-                    tail.next = node
-                    tail = tail.next
-                    right = right.next
+            if left and right and left.val<=right.val:
+                node = ListNode(left.val)
+                tail.next = node
+                tail = tail.next
+                left = left.next
+            elif left and right and left.val>right.val:
+                node = ListNode(right.val)
+                tail.next = node
+                tail = tail.next
+                right = right.next
             elif left:
                 while left:
-                    tail.next = left
+                    node = ListNode(left.val)
+                    tail.next = node
                     tail = tail.next
                     left = left.next
             else:
                 while right:
-                    tail.next = right
+                    node = ListNode(right.val)
+                    tail.next = node
                     tail = tail.next
                     right = right.next
         
         return dummy.next
+
+
+
+
 
 
         
