31825efc9a470ae1577cc5a1e203a109dff0954e
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-14 12:14:43 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.30%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,35 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        if n == 0 or n != len(matrix[0]):
+            raise ValueError("Matrix must be N by N")
+        
+        layers = n//2
+
+        for layer in range(layers):
+
+            first = layer
+            last = n - 1 - layer
+
+            for i in range(first, last):
+                offset = i - first
+
+                # save top row in top
+                top = matrix[first][i]
+
+                # replace top row with left col
+                matrix[first][i] = matrix[last - offset][first]
+                
+                # replace left col with bottom row
+                matrix[last - offset][first] = matrix[last][last - offset]
+
+                # replace bottom row with right col
+                matrix[last][last - offset] = matrix[i][last]
+            
+                # replace right col with top 
+                matrix[i][last] = top
+
+        
