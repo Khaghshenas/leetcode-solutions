d289027ddcb5f4b7725681acdcc9c156c7f66c42
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-16 13:26:43 +0100

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.93%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,22 +1,17 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-
         words = []
         current_word = ''
 
         for c in s:
-            if c==' ' and not current_word:
-                continue
-            elif c==' ' and current_word:
-                words.append(current_word)
-                current_word = ''
+            if c == ' ':
+                if current_word:
+                    words.append(current_word)
+                    current_word = ''
             else:
                 current_word += c
-        
-        if current_word:
+
+        if current_word:  # only append non-empty word at the end
             words.append(current_word)
 
-    
-        sentence = ' '.join(reversed(words))
-        return sentence
-        
+        return ' '.join(reversed(words))
