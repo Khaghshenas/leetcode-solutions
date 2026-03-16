f3bd34371748bd38232fbfb6ec3aad66e60873e8
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-16 12:41:35 +0100

    Time: 0 ms (100.00%) | Memory: 19.6 MB (30.86%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,16 +2,20 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
 
-        def backtrack(current: str, open_count: int, close_count: int):
-            if len(current) == 2 * n:
+        def generate_progressive(current: str, opening_num: int, closing_num:int):
+            nonlocal result
+            if len(current)==2*n:
                 result.append(current)
                 return
-            if open_count < n:
-                backtrack(current + '(', open_count + 1, close_count)
-            if close_count < open_count:
-                backtrack(current + ')', open_count, close_count + 1)
+            
+            if opening_num<n:
+                generate_progressive(current+'(', opening_num+1, closing_num)
+            
+            if closing_num<opening_num:
+                generate_progressive(current+')', opening_num, closing_num+1)
 
-        backtrack('', 0, 0)
+
+        generate_progressive('', 0, 0)
         return result
 
         
