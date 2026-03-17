a628c2e5e364f83893ba0e20dcce577750d22309
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-17 16:04:56 +0100

    Time: 15 ms (31.09%) | Memory: 19.3 MB (74.73%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -4,26 +4,26 @@ class Solution:
         if len(s) <= numRows or numRows == 1:
             return s
 
-        rows_val = ["" for _ in range(numRows)]
+        rows = ["" for _ in range(numRows)]
 
         current_row = 0
-        to_down = True
+        going_down = True
         
         for c in s:
-            rows_val[current_row] += c
+            rows[current_row] += c
 
             if current_row == 0:
-                to_down = True
+                going_down = True
             elif current_row == numRows - 1:
-                to_down = False
+                going_down = False
             
-            if to_down:
+            if going_down:
                 current_row += 1
             else:
                 current_row -= 1
 
         res = ""
-        for r in rows_val:
+        for r in rows:
             res = res + r
 
         return res
