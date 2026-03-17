69b2b90412dd3d73b1dec45ab4c074a0377ac9ce
Kawsar Haghshenas <39369208+Khaghshenas@users.noreply.github.com>
2026-03-17 16:04:02 +0100

    Time: 8 ms (72.58%) | Memory: 19.4 MB (74.73%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,31 +1,30 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
-        n = len(s)
-        if n <= numRows or numRows == 1:
+        if len(s) <= numRows or numRows == 1:
             return s
-        
-        matrix = [["" for _ in range(n)] for _ in range(numRows)]
 
-        row = col = 0 
+        rows_val = ["" for _ in range(numRows)]
+
+        current_row = 0
         to_down = True
-
-        for c in s:
-            matrix[row][col] = c
-
-            if row == numRows - 1:
-                to_down = False
-            elif row == 0:
-                to_down = True
-
-            if to_down:
-                row += 1
-            else:
-                row -= 1
-                col += 1
         
+        for c in s:
+            rows_val[current_row] += c
+
+            if current_row == 0:
+                to_down = True
+            elif current_row == numRows - 1:
+                to_down = False
+            
+            if to_down:
+                current_row += 1
+            else:
+                current_row -= 1
+
         res = ""
-        for r in matrix:
-            res += ''.join(r)
+        for r in rows_val:
+            res = res + r
 
         return res
+
